110c2032f09ab8100833c3f8f76424b8445c1bef
lowlevelcode <thelowlevelcode@gmail.com>
2021-05-30 03:52:55 -0600

    se agrega hash para resolver tema de rutas

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -2,12 +2,13 @@ import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 
 const routes: Routes = [
+  { path: '', redirectTo:'admin', pathMatch:'full' },
   { path: 'admin', loadChildren: () => import('./@pages/admin/admin.module').then(m => m.AdminModule) },
   { path: 'echo', loadChildren: () => import('./@pages/echo/echo.module').then(m => m.EchoModule) }
 ];
 
 @NgModule({
-  imports: [RouterModule.forRoot(routes)],
+  imports: [RouterModule.forRoot(routes, { useHash: true })],
   exports: [RouterModule]
 })
 export class AppRoutingModule { }
